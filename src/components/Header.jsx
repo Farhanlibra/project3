c1e72a1925cd205157a3e5f36ca292d1e47d83f3
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-07-08 02:02:46 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <>
-    <div>
+    <div  className={style.imageholder} >
       
       
         <nav className="navbar navbar-expand-lg " >
