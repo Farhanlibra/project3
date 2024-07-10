ce6596602bd733feb5bd00991c0374b4573fa966
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-07-10 11:38:13 -0700

    e

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
 <div className={style.contimage}>
 <p>uicookies.com Creates High Quality Bootstrap Template For Free</p>
 </div>
-<div className={style.btnbtnp}>
+<div className={style.buttonp}>
 <button   className='btn btn-primary , '>Get Them Now</button>
 <button  className='btn btn-primary ' >See Feature</button></div>
 
