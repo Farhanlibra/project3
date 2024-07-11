42c6327b2ef2d0318fbb81726f09dbf183f6f7c6
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-07-11 01:09:47 -0700

    a

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,173 +2,27 @@ import React from "react";
 
 const Features = () => {
   return (
-    <div>
-      <div className="fh" style={{ backgroundColor: "cyan" }}>
-        <h1
-          style={{
-            textAlign: "center",
-            margin: "40px",
-            backgroundColor: "cyan",
-          }}
-        >
-          Features
-        </h1>
-        <hr
-          style={{
-            border: "solid, 3px, blue",
-            width: "100px",
-            marginTop: "30px",
-            marginLeft: "700px",
-          }}
-        ></hr>
-        <p
-          style={{
-            textAlign: "center",
-            width: "680px",
-            marginLeft: "380px",
-            marginTop: "80px",
-            fontSize: "20px",
-            backgroundColor: "cyan",
-          }}
-        >
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar.
-        </p>
+    <div className="container-fluid">
+      <div className="box" id="box1">
+
       </div>
-      <div className="feature">
-        <div className="left">
-          <img className="fimage1" src="f1.jpg" alt="" />
-        </div>
-        <div className="right">
-          <h2
-            style={{
-              color: "blue",
-              marginBottom: "40px",
-              lineHeight: "60px",
-              fontSize: "40px",
-              backgroundColor: "cyan",
-            }}
-          >
-            Big Benefits for Small <br></br>Business
-          </h2>
-          <p
-            style={{
-              width: "400px",
-              fontSize: "18px",
-              marginBottom: "20px",
-              lineHeightStep: "5px",
-              backgroundColor: "cyan",
-            }}
-          >
-            A small river named Duden flows by their place and supplies it with
-            the necessary regelialia. It is a paradisematic country, in which
-            roasted parts of sentences fly into your mouth.
-          </p>
-          <ul style={{ marginTop: "30px", backgroundColor: "cyan" }}>
-            <li style={{ backgroundColor: "cyan" }}>
-              {" "}
-              Pointing has no control
-            </li>
-            <li style={{ backgroundColor: "cyan" }}>
-              {" "}
-              A small river named Duden flows
-            </li>
-            <li style={{ backgroundColor: "cyan" }}>
-              Roasted parts of sentences fly into your mouth
-            </li>
-          </ul>
-        </div>
+      <div className="box" id="box1">
+        
       </div>
-      <div className="feature">
-        <div className="left1">
-          <h2
-            style={{
-              color: "blue",
-              marginBottom: "40px",
-              lineHeight: "60px",
-              fontSize: "40px",
-              backgroundColor: "cyan",
-              float: "left",
-            }}
-          >
-            How Frame is different
-          </h2>
-          <p
-            style={{
-              width: "400px",
-              fontSize: "18px",
-              marginBottom: "20px",
-              lineHeight: "30px",
-              backgroundColor: "cyan",
-            }}
-          >
-            A small river named Duden flows by their place and supplies it with
-            the necessary regelialia. It is a paradisematic country, in which
-            roasted parts of sentences fly into your mouth.
-          </p>
-          <ul
-            style={{
-              marginTop: "30px",
-              fontSize: "20px",
-              lineHeight: "40px",
-              backgroundColor: "cyan",
-            }}
-          >
-            <li style={{ backgroundColor: "cyan" }}>Pointing has no control</li>
-            <li style={{ backgroundColor: "cyan" }}>
-              A small river named Duden flows
-            </li>
-            <li style={{ backgroundColor: "cyan" }}>
-              {" "}
-              Roasted parts of sentences fly into your mouth
-            </li>
-          </ul>
-        </div>
-        <div className="right1" style={{ marginLeft: "160px" }}>
-          <img className="fimage1" src="f2.jpg" alt="" />
-        </div>
+      <div className="box" id="box2">
+        
       </div>
-      <div className="feature">
-        <div className="left">
-          <img className="fimage1" src="f1.jpg" alt="" />
-        </div>
-        <div className="right">
-          <h2
-            style={{
-              color: "blue",
-              marginBottom: "40px",
-              lineHeight: "60px",
-              fontSize: "40px",
-              backgroundColor: "cyan",
-            }}
-          >
-            All in One Place
-          </h2>
-          <p
-            style={{
-              width: "400px",
-              fontSize: "18px",
-              marginBottom: "20px",
-              lineHeightStep: "5px",
-              backgroundColor: "cyan",
-            }}
-          >
-            A small river named Duden flows by their place and supplies it with
-            the necessary regelialia. It is a paradisematic country, in which
-            roasted parts of sentences fly into your mouth.
-          </p>
-          <ul style={{ marginTop: "30px", backgroundColor: "cyan" }}>
-            <li style={{ backgroundColor: "cyan" }}>Pointing has no control</li>
-            <li style={{ backgroundColor: "cyan" }}>
-              A small river named Duden flows
-            </li>
-            <li style={{ backgroundColor: "cyan" }}>
-              Roasted parts of sentences fly into your mouth
-            </li>
-          </ul>
-        </div>
+      <div className="box" id="box3">
+        
+      </div>
+      <div className="box" id="box4">
+        
+      </div>
+      <div className="box" id="box5">
+        
+      </div>
+      <div className="box" id="box6">
+        
       </div>
     </div>
   );
