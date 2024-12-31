0099b3719cdd5a8c5c4e35e7a56b689cc3376bec
farhanlibra1984 <farhanlibra1984@outlook.com>
2024-12-30 23:18:15 -0800

    Update Aldhahery.jsx

--- src/components/Aldhahery.jsx
+++ src/components/Aldhahery.jsx
@@ -1,10 +1,11 @@
-const Aldhahery=()=>{
-    return(
-        <>
-        <h1>gift record</h1>
-        </>
+import React from 'react'
 
-    )
+const Aldhahery = () => {
+  return (
+    <div>
+        <h1>AL DHAHERY GIFTS RECORD</h1>
+    </div>
+  )
 }
 
 export default Aldhahery
